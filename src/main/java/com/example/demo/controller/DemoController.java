0986de2d3068a2b6c418b43ab5fc94a912349a0c
ashish <ashishmahanta121@gmail.com>
2026-03-26 01:31:35 +0530

    one fun added

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
-    @GetMapping("hy")
+    @GetMapping("hyy")
     public  String hy(){
         return "hyy we are accessing backend";
     }
